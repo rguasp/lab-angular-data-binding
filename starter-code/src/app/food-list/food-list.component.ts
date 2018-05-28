0860189bda1537c6158b35d3b582ece080ae2909
Rafael Guasp <ralfyguasp@gmail.com>
2018-05-27 23:58:28 -0400

    done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,58 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  searchTerm: String = "";
+  resultsArray: Array<any> = [];
+  theFood: Array<any> = [];
+  newFood: any = {
+    name:"",
+    calories:"",
+    image:"",
+    quantity:""
+  };
+  todaysFood: Array<any> = [];
+  // theTotalCalories: number = totalColories;
+  todaysCalories:number = 0;
+  openform:boolean=false;
   constructor() { }
 
   ngOnInit() {
+    this.theFood = foods;
+    this.resultsArray = this.theFood;
+  }
+
+  addFood(){
+    // const theNewFood = {
+    //   name: this.newFood.name,
+    //   calories:this.newFood.calories,
+    //   image:this.newFood.image,
+    //   quantity:this.newFood.quantity
+    // }
+    this.theFood.unshift(this.newFood);
+    // this.newFood = {
+    //   name:"",
+    //   calories:"",
+    //   image:"",
+    //   quantity:""
+    // }
+    this.openform=false;
+  }
+  onClickOpenForm(){
+    if(this.openform === false){
+      this.openform=true;  
+    } else if (this.openform === true)
+      this.openform=false;
+  }
+
+  addTodaysFood(food){
+    this.todaysFood.unshift(food);
+    this.todaysCalories+= Number(food.calories);
+  }
+
+  filterFoods(){
+    this.resultsArray = this.theFood.filter((food=>{
+      return food.name.includes(this.searchTerm);
+    }))
   }
 
 }
